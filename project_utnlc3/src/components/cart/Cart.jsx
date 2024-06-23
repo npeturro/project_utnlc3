02370516d408e2862721b2a295be2369e2258657
npeturro <npeturro@gmail.com>
2024-06-23 17:58:02 -0300

    intregracion con mercado pago

--- project_utnlc3/src/components/cart/Cart.jsx
+++ project_utnlc3/src/components/cart/Cart.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import Checkout from '../checkout/Checkout';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart-context';
+import axios from 'axios';
 
 function Cart() {
 
@@ -18,8 +19,11 @@ function Cart() {
         ) {
             return;
         }
+
+        console.log(open)
         setDrawerOpen(open);
     };
+    const navigate = useNavigate();
 
     const handleQuantityChange = (id, value) => {
         updateQuantity(id, value);
@@ -31,78 +35,129 @@ function Cart() {
 
     const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
 
+    const [preferenceId, setPreferenceId] = useState('');
+
+    const createPreference = async (total) => {
+        try {
+            const response = await axios.post(
+                "http://onetechapi-utn.ddns.net/api/Payments/create-preference",
+                {
+                    title: "Compra",
+                    quantity: 1,
+                    unitPrice: total,
+                    currencyId: "ARS"
+                }
+            );
+    
+           
+            return response.data.id;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleBuy = async (total, cart) => {
+        const id = await createPreference(total);
+        console.log(id)
+        navigate("/Checkout", { state: { id, total, cart } })
+    };
+
+
+
     return (
-        <Box sx={{ p: 5 }}>
+        <Box sx={{ p: 5, mt: 8 }}>
             <Grid container justifyContent="center" alignItems="center" mb={4}>
                 <Typography variant="h4" gutterBottom>CARRITO DE COMPRAS</Typography>
             </Grid>
-            <Box sx={{ flexGrow: 1, overflowY: 'auto', position: 'relative', minHeight: '600px' }}>
-                {cart.length >= 1 ? (
-                    <Grid container spacing={2} sx={{ mb: 4 }}>
-                        {cart.map((item) => (
-                            <Grid item xs={12} key={item.id}>
-                                <Card sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
-                                    <CardMedia
-                                        component="img"
-                                        sx={{
-                                            width: 100,
-                                            height: 100,
-                                            mr: 2
-                                        }}
-                                        image={item.image}
-                                        alt={item.name}
-                                    />
+            {
+                cart.length >= 1 ? (
+                    <Box sx={{ flexGrow: 1, overflowY: 'auto', position: 'relative', minHeight: '600px' }}>
+                        <Grid container spacing={2} sx={{ mb: 4 }}>
+                            {cart.map((item) => (
+                                <Grid item xs={12} key={item.id}>
+                                    <Card sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+                                        <CardMedia
+                                            component="img"
+                                            sx={{
+                                                width: 100,
+                                                height: 100,
+                                                mr: 2
+                                            }}
+                                            image={item.image}
+                                            alt={item.name}
+                                        />
 
-                                    <CardContent sx={{ flex: '1 0 auto', display: 'flex', alignItems: 'center' }}>
-                                        <Typography component="div" variant="h6" sx={{ flex: '1', minWidth: 0, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{item.name}</Typography>
-                                        <Typography variant="subtitle1" color="text.secondary" component="div" sx={{ flex: '1', textAlign: 'center', minWidth: 0, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
-                                            {item.price.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}
-                                        </Typography>
-                                        <Box sx={{ display: 'flex', alignItems: 'center', flex: 1 }}>
-                                            <Box sx={{ display: 'flex', alignItems: 'center', border: '1px solid #ccc', borderRadius: '4px', p: 0.5 }}>
-                                                <IconButton onClick={() => handleQuantityChange(item.id, -1)} disabled={item.quantity === 1}>
-                                                    <Remove />
-                                                </IconButton>
-                                                <TextField
-                                                    type="text"
-                                                    value={item.quantity}
-                                                    InputProps={{
-                                                        readOnly: true,
-                                                        disableUnderline: true,
-                                                        sx: {
-                                                            textAlign: 'center',
-                                                            '& input': {
+                                        <CardContent sx={{ flex: '1 0 auto', display: 'flex', alignItems: 'center' }}>
+                                            <Typography component="div" variant="h6" sx={{ flex: '1', minWidth: 0, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{item.name}</Typography>
+                                            <Typography variant="subtitle1" color="text.secondary" component="div" sx={{ flex: '1', textAlign: 'center', minWidth: 0, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                                                {item.price.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}
+                                            </Typography>
+                                            <Box sx={{ display: 'flex', alignItems: 'center', flex: 1 }}>
+                                                <Box sx={{ display: 'flex', alignItems: 'center', border: '1px solid #ccc', borderRadius: '4px', p: 0.5 }}>
+                                                    <IconButton onClick={() => handleQuantityChange(item.id, -1)} disabled={item.quantity === 1}>
+                                                        <Remove />
+                                                    </IconButton>
+                                                    <TextField
+                                                        type="text"
+                                                        value={item.quantity}
+                                                        InputProps={{
+                                                            readOnly: true,
+                                                            disableUnderline: true,
+                                                            sx: {
                                                                 textAlign: 'center',
-                                                                padding: 0,
-                                                                height: 'auto',
+                                                                '& input': {
+                                                                    textAlign: 'center',
+                                                                    padding: 0,
+                                                                    height: 'auto',
+                                                                },
                                                             },
-                                                        },
-                                                    }}
-                                                    variant="standard"
-                                                    size="small"
-                                                    sx={{
-                                                        width: 50,
-                                                        display: 'flex',
-                                                        alignItems: 'center',
-                                                        justifyContent: 'center',
-                                                    }}
-                                                />
-                                                <IconButton onClick={() => handleQuantityChange(item.id, 1)}>
-                                                    <Add />
+                                                        }}
+                                                        variant="standard"
+                                                        size="small"
+                                                        sx={{
+                                                            width: 50,
+                                                            display: 'flex',
+                                                            alignItems: 'center',
+                                                            justifyContent: 'center',
+                                                        }}
+                                                    />
+                                                    <IconButton onClick={() => handleQuantityChange(item.id, 1)}>
+                                                        <Add />
+                                                    </IconButton>
+                                                </Box>
+                                                <IconButton onClick={() => handleRemoveItem(item.id)} sx={{ ml: 1 }}>
+                                                    <Delete />
                                                 </IconButton>
                                             </Box>
-                                            <IconButton onClick={() => handleRemoveItem(item.id)} sx={{ ml: 1 }}>
-                                                <Delete />
-                                            </IconButton>
-                                        </Box>
-                                        <Typography variant="h6" sx={{ flex: '1', textAlign: 'right', minWidth: 0, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
-                                            {(item.price * item.quantity).toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
+                                            <Typography variant="h6" sx={{ flex: '1', textAlign: 'right', minWidth: 0, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                                                {(item.price * item.quantity).toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+
+                        <div style={{ position: 'absolute', bottom: 0, left: 0, width: '100%', textAlign: 'center' }}>
+                            <Card sx={{ p: 2 }}>
+                                <Typography variant="h7">Subtotal: {total.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}</Typography>
+                                <Typography variant="h6" fontWeight="bold" sx={{ mt: 2 }}>Total: {total.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}</Typography>
+                                <Button variant="contained"
+                                    sx={{
+                                        mt: 2,
+                                        backgroundColor: '#051c67',
+                                        borderRadius: '50px',
+                                        '&:hover': {
+                                            backgroundColor: '#051c40',
+                                        }
+                                    }}
+                                    onClick={() => handleBuy(total, cart)}
+                                >
+                                    Ir a pagar
+                                </Button>
+                            </Card>
+                        </div>
+                    </Box>
                 ) : (
                     <Grid container justifyContent="center">
                         <Grid item xs={12} md={6}>
@@ -112,6 +167,7 @@ function Cart() {
                                         <ShoppingCart fontSize='large' />
                                         <Typography>Tu carrito está vacío</Typography>
                                     </CardContent>
+
                                 </Card>
                                 <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
                                     <Button
@@ -123,44 +179,23 @@ function Cart() {
                             </div>
                         </Grid>
                     </Grid>
-                )}
+                )
+            }
 
-                <div style={{ position: 'absolute', bottom: 0, left: 0, width: '100%', textAlign: 'center' }}>
-                    <Card sx={{ p: 2 }}>
-                        <Typography variant="h7">Subtotal: {total.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}</Typography>
-                        <Typography variant="h6" fontWeight="bold" sx={{ mt: 2 }}>Total: {total.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}</Typography>
-                        <Button variant="contained"
-                            sx={{
-                                mt: 2,
-                                backgroundColor: '#051c67',
-                                borderRadius: '50px',
-                                '&:hover': {
-                                    backgroundColor: '#051c40',
-                                }
-                            }}
-                            onClick={() => toggleDrawer(true)}
-                        >
-                            Realizar pedido
-                        </Button>
-                    </Card>
-                </div>
-            </Box>
-
-            <Drawer
+            {/* <Drawer
                 anchor="right"
                 open={drawerOpen}
-                onClose={() => toggleDrawer(false)}
+                onClose={toggleDrawer(false)}
             >
                 <Box
                     sx={{ width: 500 }}
                     role="presentation"
-                    onClick={() => toggleDrawer(false)}
-                    onKeyDown={() => toggleDrawer(false)}
+                    onClick={toggleDrawer(false)}
+                    onKeyDown={toggleDrawer(false)}
                 >
-                    {/* Componente Checkout */}
-                    <Checkout products={cart} />
+                    <Checkout products={cart} preferenceId={preferenceId}  />
                 </Box>
-            </Drawer>
+            </Drawer> */}
         </Box>
     );
 }
