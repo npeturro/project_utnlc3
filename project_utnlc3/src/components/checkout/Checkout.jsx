23f43bb165e14a009911be7fd885c666df19d589
Matías Volpe <mvolpe@neosistemassrl.com>
2024-07-22 12:45:00 -0300

    Prueba de commit anterior

--- project_utnlc3/src/components/checkout/Checkout.jsx
+++ project_utnlc3/src/components/checkout/Checkout.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect, useCallback } from "react";
+import { useState, useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import {
     Box,
